d7b1ca17771cdf0629730d6f9a2b97cd647a9b05
Blundell <blundell.app.dev@gmail.com>
2013-06-23 13:23:31 +0100

    Made it obvious what the interface uses

--- core/src/main/java/com/novoda/sexp/parser/ListParseWatcher.java
+++ core/src/main/java/com/novoda/sexp/parser/ListParseWatcher.java
@@ -2,6 +2,15 @@ package com.novoda.sexp.parser;
 
 import android.sax.ElementListener;
 
-public interface ListParseWatcher<T> extends ParseWatcher<T>, ElementListener {
+import org.xml.sax.Attributes;
 
+public interface ListParseWatcher<T> extends ParseWatcher<T>, ElementListener {
+    @Override
+    void start(Attributes attributes);
+
+    @Override
+    void onParsed(T item);
+
+    @Override
+    void end();
 }
