39f5cc36ce7ae15d85606d05c46e0c2a58bbb765
Adam Brown <adam@novoda.com>
2013-11-26 23:31:01 +0000

    using better name for internal default xml creation method

--- sexp/src/main/java/com/novoda/sexp/SEXPXmlReaderFactory.java
+++ sexp/src/main/java/com/novoda/sexp/SEXPXmlReaderFactory.java
@@ -5,10 +5,10 @@ import org.xml.sax.XMLReader;
 class SEXPXmlReaderFactory {
 
     public static XMLReader getDefault() throws XMLReaderBuilder.XMLReaderCreationException {
-        return new SEXPXmlReaderFactory().getDefaultXmlReader();
+        return new SEXPXmlReaderFactory().createDefaultXmlReader();
     }
 
-    private XMLReader getDefaultXmlReader() throws XMLReaderBuilder.XMLReaderCreationException {
+    private XMLReader createDefaultXmlReader() throws XMLReaderBuilder.XMLReaderCreationException {
         XMLReaderBuilder builder = new XMLReaderBuilder();
         builder.withFeature("http://xml.org/sax/features/namespaces", true);
         return builder.build();
