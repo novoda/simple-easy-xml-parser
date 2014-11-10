59d94e157b2a52f554360bfcbf849040eb9377e5
Adam Brown <adam@novoda.com>
2014-11-10 14:06:05 +0000

    applying the novoda formatting to an external pr

--- sexp/src/test/java/com/novoda/sax/ChildrenShould.java
+++ sexp/src/test/java/com/novoda/sax/ChildrenShould.java
@@ -4,50 +4,37 @@ import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
+public class ChildrenShould {
 
-public class ChildrenShould
-{
-    public static final String FIRST_STRING_WITH_EQUAL_HASH = "0-42L";
-
-    public static final String SECOND_STRING_WITH_EQUAL_HASH = "0-43-";
+    private static final String FIRST_STRING_WITH_EQUAL_HASH = "0-42L";
+    private static final String SECOND_STRING_WITH_EQUAL_HASH = "0-43-";
 
     private Children children = new Children();
-
-    private Element parent = new Element( null, "", "test", 0 );
-
+    private Element parent = new Element(null, "", "test", 0);
 
     @Test
-    public void create_new_child_with_equal_hash_uri_and_local()
-        throws Exception
-    {
-        Element element = children.getOrCreate( parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH );
+    public void create_new_child_with_equal_hash_uri_and_local() throws Exception {
+        Element element = children.getOrCreate(parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH);
+        Element anotherElement = children.getOrCreate(parent, SECOND_STRING_WITH_EQUAL_HASH, SECOND_STRING_WITH_EQUAL_HASH);
 
-        Element anotherElement =
-            children.getOrCreate( parent, SECOND_STRING_WITH_EQUAL_HASH, SECOND_STRING_WITH_EQUAL_HASH );
-
-        assertThat( element ).isNotSameAs( anotherElement );
+        assertThat(element).isNotSameAs(anotherElement);
     }
 
     @Test
-    public void same_child_with_equal_uri_and_local()
-    {
-        Element element = children.getOrCreate( parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH );
-        Element anotherElement =
-            children.getOrCreate( parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH );
+    public void same_child_with_equal_uri_and_local() {
+        Element element = children.getOrCreate(parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH);
+        Element anotherElement = children.getOrCreate(parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH);
 
-        assertThat( element ).isSameAs( anotherElement );
+        assertThat(element).isSameAs(anotherElement);
     }
 
     @Test
-    public void return_proper_child_with_equal_hash_uri_and_local()
-    {
-        children.getOrCreate( parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH );
+    public void return_proper_child_with_equal_hash_uri_and_local() {
+        children.getOrCreate(parent, FIRST_STRING_WITH_EQUAL_HASH, FIRST_STRING_WITH_EQUAL_HASH);
 
-        Element anotherElement =
-            children.getOrCreate( parent, SECOND_STRING_WITH_EQUAL_HASH, SECOND_STRING_WITH_EQUAL_HASH );
+        Element anotherElement = children.getOrCreate(parent, SECOND_STRING_WITH_EQUAL_HASH, SECOND_STRING_WITH_EQUAL_HASH);
 
-        assertThat( children.get( SECOND_STRING_WITH_EQUAL_HASH, SECOND_STRING_WITH_EQUAL_HASH ) ).isSameAs(
-            anotherElement );
+        assertThat(children.get(SECOND_STRING_WITH_EQUAL_HASH, SECOND_STRING_WITH_EQUAL_HASH)).isSameAs(anotherElement);
     }
 
 }
