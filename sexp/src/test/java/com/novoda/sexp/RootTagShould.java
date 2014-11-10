d8fa37d4ee444e5cf4a810b8c7f57d64b049fe1c
Adam Brown <adam@novoda.com>
2014-11-10 14:31:52 +0000

    applying novoda style to test class

--- sexp/src/test/java/com/novoda/sexp/RootTagShould.java
+++ sexp/src/test/java/com/novoda/sexp/RootTagShould.java
@@ -6,19 +6,21 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class RootTagShould {
 
-    private String root = "root";
-    private String namespace = "namespace";
+    private static final String ROOT = "root";
+    private static final String NAMESPACE = "namespace";
 
     @Test
     public void setRoot_onCreate() throws Exception {
-        RootTag rootTag = RootTag.create(root, namespace);
-        assertThat(rootTag.getTag()).isEqualTo(root);
+        RootTag rootTag = RootTag.create(ROOT, NAMESPACE);
+
+        assertThat(rootTag.getTag()).isEqualTo(ROOT);
     }
 
     @Test
     public void setNamespace_onCreate() throws Exception {
-        RootTag rootTag = RootTag.create(root, namespace);
-        assertThat(rootTag.getNamespace()).isEqualTo(namespace);
+        RootTag rootTag = RootTag.create(ROOT, NAMESPACE);
+
+        assertThat(rootTag.getNamespace()).isEqualTo(NAMESPACE);
     }
 
 }
