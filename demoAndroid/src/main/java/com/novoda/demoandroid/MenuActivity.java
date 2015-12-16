c513d08035becc15bdbf98dba7e7ece51edf9905
Paul Blundell <paul@novoda.com>
2015-12-16 20:35:26 +0000

    Adds a demo for parsing with return value

--- demoAndroid/src/main/java/com/novoda/demoandroid/MenuActivity.java
+++ demoAndroid/src/main/java/com/novoda/demoandroid/MenuActivity.java
@@ -1,8 +1,5 @@
 package com.novoda.demoandroid;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -13,69 +10,75 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.novoda.demoandroid.returnvalue.ReturnValueActivity;
 import com.novoda.demoandroid.onetag.OneTagActivity;
 import com.novoda.demoandroid.podcast.PodcastExampleActivity;
 import com.novoda.demoandroid.simple.SimpleExampleActivity;
 import com.novoda.demoandroid.team.TeamExampleActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuActivity extends Activity {
-	private static final int DIVIDER_HEIGHT = 2;
-	private ListView menu;
-	private Context context;
+    private static final int DIVIDER_HEIGHT = 2;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
-		
-		context = this;
-		menu = (ListView) findViewById(R.id.lv_menu);
-		List<NavigationItem> items = populateNavigationMenu();
-		ArrayAdapter<NavigationItem> adapter;
-		adapter = new ArrayAdapter<NavigationItem>(this, android.R.layout.simple_list_item_1, android.R.id.text1, items);
+    private Context context;
 
-		menu.setAdapter(adapter);
-		menu.setDividerHeight(DIVIDER_HEIGHT);
-		menu.setOnItemClickListener(new OnItemClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				NavigationItem current = (NavigationItem) parent.getItemAtPosition(position);
-				Intent intent = new Intent(context, current.getType());
-				startActivity(intent);
-			}
-		});
-	}
-	
-	private List<NavigationItem> populateNavigationMenu (){
-		List<NavigationItem> items = new ArrayList<NavigationItem>();
-		items.add(new NavigationItem(OneTagActivity.class, getResources().getString(R.string.title_activity_one_tag)));
-		items.add(new NavigationItem(SimpleExampleActivity.class, getResources().getString(R.string.title_activity_simple_example)));
-		items.add(new NavigationItem(PodcastExampleActivity.class, getResources().getString(R.string.title_activity_podcast)));
-		items.add(new NavigationItem(TeamExampleActivity.class, getResources().getString(R.string.title_activity_team_example)));
-		return items;
-	}
+        context = this;
+        ListView menu = (ListView) findViewById(R.id.lv_menu);
+        List<NavigationItem> items = populateNavigationMenu();
+        ArrayAdapter<NavigationItem> adapter;
+        adapter = new ArrayAdapter<NavigationItem>(this, android.R.layout.simple_list_item_1, android.R.id.text1, items);
 
-	public static class NavigationItem {
-		Class<? extends Activity> type;
-		String title;
+        menu.setAdapter(adapter);
+        menu.setDividerHeight(DIVIDER_HEIGHT);
+        menu.setOnItemClickListener(
+                new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        NavigationItem current = (NavigationItem) parent.getItemAtPosition(position);
+                        Intent intent = new Intent(context, current.getType());
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
 
-		public Class<? extends Activity> getType() {
-			return type;
-		}
+    private List<NavigationItem> populateNavigationMenu() {
+        List<NavigationItem> items = new ArrayList<NavigationItem>();
+        items.add(new NavigationItem(OneTagActivity.class, getResources().getString(R.string.title_activity_one_tag)));
+        items.add(new NavigationItem(ReturnValueActivity.class, getResources().getString(R.string.title_activity_return_value)));
+        items.add(new NavigationItem(SimpleExampleActivity.class, getResources().getString(R.string.title_activity_simple_example)));
+        items.add(new NavigationItem(PodcastExampleActivity.class, getResources().getString(R.string.title_activity_podcast)));
+        items.add(new NavigationItem(TeamExampleActivity.class, getResources().getString(R.string.title_activity_team_example)));
+        return items;
+    }
 
-		public String getTitle() {
-			return title;
-		}
+    public static class NavigationItem {
+        Class<? extends Activity> type;
+        String title;
 
-		public NavigationItem(Class<? extends Activity> type, String title) {
-			this.type = type;
-			this.title = title;
-		}
+        public Class<? extends Activity> getType() {
+            return type;
+        }
 
-		@Override
-		public String toString() {
-			return title;
-		}
-	}
+        public String getTitle() {
+            return title;
+        }
+
+        public NavigationItem(Class<? extends Activity> type, String title) {
+            this.type = type;
+            this.title = title;
+        }
+
+        @Override
+        public String toString() {
+            return title;
+        }
+    }
 }
