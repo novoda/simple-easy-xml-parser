8850d4e9acb87ac431284a84515a3ab1db8c924f
Paul Blundell <paul@novoda.com>
2015-12-16 19:45:13 +0000

    explains the Instigator methods

--- sexp/src/main/java/com/novoda/sexp/finder/ElementFinder.java
+++ sexp/src/main/java/com/novoda/sexp/finder/ElementFinder.java
@@ -1,11 +1,25 @@
 package com.novoda.sexp.finder;
 
 import com.novoda.sax.Element;
-
 import com.novoda.sexp.parser.ParseWatcher;
 
 public interface ElementFinder<R> extends ParseWatcher<R> {
+
+    /**
+     * Find the given tag inside the given element
+     *
+     * @param from the XML element to search within
+     * @param tag  the XML tag to be searched for
+     */
     void find(Element from, String tag);
+
+    /**
+     * Find the given tag inside the given element
+     *
+     * @param from the XML element to search within
+     * @param uri  the XML namespace of the searched for tag
+     * @param tag  the XML tag to be searched for
+     */
     void find(Element from, String uri, String tag);
 
     @Override
