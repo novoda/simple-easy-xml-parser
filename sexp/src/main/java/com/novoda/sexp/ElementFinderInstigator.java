845fb120a10ac872d18dd3e5c6ce27d6de569efc
Paul Blundell <paul@novoda.com>
2015-12-16 20:38:41 +0000

    explain the new instigator
     - this is used so we can access the element finder that is passed to the instigator (usually under the clients control)

--- sexp/src/main/java/com/novoda/sexp/ElementFinderInstigator.java
+++ sexp/src/main/java/com/novoda/sexp/ElementFinderInstigator.java
@@ -3,6 +3,12 @@ package com.novoda.sexp;
 import com.novoda.sax.RootElement;
 import com.novoda.sexp.finder.ElementFinder;
 
+/**
+ * This instigator can be used when you want to return your parsed object synchronously
+ * from the element finder. A good example being {@link SimpleEasyXmlParser#parse(String, ElementFinderInstigator)}
+ *
+ * @param <R> the type of object you expect as a result of xml parsing
+ */
 public abstract class ElementFinderInstigator<R> implements Instigator {
 
     private final ElementFinder<R> elementFinder;
@@ -23,6 +29,9 @@ public abstract class ElementFinderInstigator<R> implements Instigator {
         // no callback on completion
     }
 
+    /**
+     * @return the object you expected to be parsed from your dependent {@link ElementFinder<R>}
+     */
     public R getResultOrThrow() {
         return elementFinder.getResultOrThrow();
     }
